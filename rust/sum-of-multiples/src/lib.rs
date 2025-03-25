69145b0c299d6c7a2cbf4c247c0e42415b104af0
Rob Hand <146272+sinon@users.noreply.github.com>
2025-03-25 18:44:53 +0000

    Rust: Sum of Multiples

---
+++ rust/sum-of-multiples/src/lib.rs
@@ -0,0 +1,11 @@
+use std::collections::HashSet;
+
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    factors
+        .iter()
+        .filter(|f| **f > 0)
+        .flat_map(|f| (*f..limit).step_by(*f as usize).collect::<HashSet<u32>>())
+        .collect::<HashSet<_>>()
+        .iter()
+        .sum()
+}
